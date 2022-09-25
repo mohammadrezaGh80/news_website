a94a310c6d43ce67e732a8307b32c11545b0e209
Mohammadreza Gharghabi <mohammadreza.gharghabi6@gmail.com>
2022-09-25 11:42:20 +0330

    add ReportDetailView and add get_absolute_url for Report model

--- news/urls.py
+++ news/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.ReportListView.as_view(), name="report_list"),
+    path('<int:pk>/', views.ReportDetailView.as_view(), name="report_detail"),
 ]
--- news/views.py
+++ news/views.py
@@ -7,3 +7,9 @@ class ReportListView(generic.ListView):
     model = Report
     template_name = "news/report_list.html"
     context_object_name = "reports"
+
+
+class ReportDetailView(generic.DetailView):
+    model = Report
+    template_name = "news/report_detail.html"
+    context_object_name = "report"
