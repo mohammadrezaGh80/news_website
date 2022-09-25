3094089a1049ef61566844eeecc58f8bde5ed1da
Mohammadreza Gharghabi <mohammadreza.gharghabi6@gmail.com>
2022-09-25 10:18:03 +0330

    add Report model to news app

--- news/models.py
+++ news/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Report(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    author = models.CharField(max_length=200)
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
