70ffe8132a11d19180193577a25ffb69ac00b25c
Mohammadreza Gharghabi <mohammadreza.gharghabi6@gmail.com>
2022-11-23 09:35:52 +0330

    update templates

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,9 +5,16 @@ from .models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+
+        for field_name in ['username', 'password1', 'password2']:
+            self.fields[field_name].help_text = None
+
     class Meta:
         model = CustomUser
         fields = ("username", "phone_number", "age", "email")
+
 
 
 class CustomUserChangeForm(UserChangeForm):
