2fa18b63da6da223be367dbe8ec9c3f999493286
Mohammadreza Gharghabi <mohammadreza.gharghabi6@gmail.com>
2022-11-24 10:35:51 +0330

    change author to ForeignKey Field in news model

--- news/models.py
+++ news/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 
 class Report(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    author = models.CharField(max_length=200)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="reports")
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
 
