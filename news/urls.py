3c4e29472d0440db197c9d822655398a07c004ab
Mohammadreza Gharghabi <mohammadreza.gharghabi6@gmail.com>
2022-11-24 10:18:00 +0330

    add form for report create and update,sort news based on the newest and update report_create_view with its template

--- news/urls.py
+++ news/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.ReportListView.as_view(), name="report_list"),
     path('<int:pk>/', views.ReportDetailView.as_view(), name="report_detail"),
-    path('create/', views.ReportCreateView.as_view(), name="report_create"),
+    path('create/', views.report_create_view, name="report_create"),
     path('<int:pk>/update/', views.ReportUpdateView.as_view(), name="report_update"),
     path('<int:pk>/delete/', views.ReportDeleteView.as_view(), name="report_delete"),
 ]
--- news/views.py
+++ news/views.py
@@ -1,13 +1,17 @@
 from django.views import generic
 from django.urls import reverse_lazy
+from django.shortcuts import render, redirect
 
 from .models import Report
+from .forms import ReportForm
 
 
 class ReportListView(generic.ListView):
-    model = Report
     template_name = "news/report_list.html"
     context_object_name = "reports"
+
+    def get_queryset(self):
+        return Report.objects.order_by("-datetime_modified")
 
 
 class ReportDetailView(generic.DetailView):
@@ -16,10 +20,22 @@ class ReportDetailView(generic.DetailView):
     context_object_name = "report"
 
 
-class ReportCreateView(generic.CreateView):
-    model = Report
-    fields = ["title", "description", "author"]
-    template_name = "news/report_create_and_update.html"
+# class ReportCreateView(generic.CreateView):
+#     model = Report
+#     fields = ["title", "description", "author"]
+#     template_name = "news/report_create_and_update.html"
+
+def report_create_view(request):
+    if request.method == "POST":
+        form = ReportForm(request.POST)
+        if form.is_valid():
+            report = form.save(commit=False)
+            report.author = request.user
+            report.save()
+            return redirect("report_detail", report.id)
+    else:
+        form = ReportForm()
+    return render(request, "news/report_create_and_update.html", context={"form": form})
 
 
 class ReportUpdateView(generic.UpdateView):
