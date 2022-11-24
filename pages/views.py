ba39768f3d0f8d954d5c5d38c001f0ac70bc98fb
Mohammadreza Gharghabi <mohammadreza.gharghabi6@gmail.com>
2022-11-24 19:41:27 +0330

    change view for HomePageView and show latest news in home page

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,12 @@
-from django.views.generic.base import TemplateView
+from django.views import generic
+
+from news.models import Report
 
 
-class HomePageView(TemplateView):
+class HomePageView(generic.ListView):
     template_name = "home.html"
+    context_object_name = "last_news"
+
+    def get_queryset(self):
+        return Report.objects.order_by("-datetime_modified")[:3]
+
